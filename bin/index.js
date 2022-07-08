45d4372db5df2c07a92878f6590798b15dbe5db8
Denis Elianovsky <denis@opium.pro>
2022-07-08 22:10:36 +0200

    init

---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import fs from 'fs'
+import { hideBin, toSnakeCase, extractParam } from './utils.js'
+
+
+const files = hideBin(process.argv)
+
+let target = extractParam(files, '-i') || 'index.js'
+let extention = extractParam(files, '-x') || target.split('.').slice(-1).join()
+let exportFolders = extractParam(files, '-f') || false
+
+const result = {}
+
+for (const filePath of files) {
+  const parentPath = filePath.split('/').slice(0, -1).join('/')
+  const fileName = filePath.split('/').slice(-1).join('/').split('.').slice(0, -1).join('.')
+  const targetPath = `${parentPath}/${target}`
+  const parentFolder = parentPath.split('/').slice(-1).join()
+  const grandParentFolder = parentPath.split('/').slice(0, -1).join('/')
+  const parentTargetPath = `${grandParentFolder}/${target}`
+
+  if (grandParentFolder && exportFolders) {
+    result[parentTargetPath] ||= []
+    result[parentTargetPath].unshift(`export * as ${toSnakeCase(parentFolder)} from './${parentFolder}/${target}'\n`)
+  }
+
+  if (fileName === target.split('.').slice(0, -1).join('.')) {
+    continue
+  }
+
+  result[targetPath] ||= []
+  result[targetPath].push(`export * from './${fileName}.${extention}'`)
+}
+
+for (const targetPath in result) {
+  const text = result[targetPath]
+  fs.writeFileSync(targetPath, Array.from(new Set(text)).join('\n'))
+}
---
+++ bin/utils.js
@@ -0,0 +1,17 @@
+export function hideBin(argv) {
+  return argv.slice(process.versions.electron ? 0 : 1 + 1);
+}
+
+export function toSnakeCase(text) {
+  return text.replaceAll(/[\W_]+/g, '_')
+}
+
+export function extractParam(list, key, boolean = false) {
+  const targetIndex = list.indexOf(key)
+  let result
+  if (targetIndex >= 0) {
+    result = list[targetIndex + boolean ? 0 : 1]
+    list.splice(targetIndex, boolean ? 1 : 2)
+  }
+  return result
+}
